93423c792a6c10db0d392b8a288d3d076e7c9cca
Giada Pierucci <g.pierucci4@campus.uniurb.it>
2026-07-12 10:20:36 +0200

    Update features pages

---
+++ lib/features/measurements/measurements_page.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+class MeasurementPage extends StatelessWidget {
+  const MeasurementPage({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return const Center(
+      child: Text(
+        'Misurazioni',
+      ),
+    );
+  }
+}
