623e4fb7eda0041216c2b354fc079ee70d849068
Giada Pierucci <g.pierucci4@campus.uniurb.it>
2026-07-12 10:45:36 +0200

    Create feature patient page

---
+++ lib/features/patients/patient_page.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+class PatientPage extends StatelessWidget {
+  const PatientPage({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return const Center(
+      child: Text(
+        'Patients',
+      ),
+    );
+  }
+}
