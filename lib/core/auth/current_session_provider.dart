bb8cffa495aeb53b94f03e8b16374facedff5ddf
Giada Pierucci <g.pierucci4@campus.uniurb.it>
2026-07-14 21:40:17 +0200

    Rename

--- lib/core/auth/current_session_provider.dart
+++ lib/core/auth/current_session_provider.dart
@@ -3,7 +3,7 @@ import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'current_session.dart'; 
 
 final currentSessionProvider = 
-    Provider<SessionManager>(
+    Provider<CurrentSession>(
 
         (ref) {
 
